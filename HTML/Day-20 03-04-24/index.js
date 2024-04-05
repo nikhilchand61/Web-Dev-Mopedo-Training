5bff577bef5f48cc7d450d910bc7298d3871ea72
nikhilchand <nikhilchand61@gmail.com>
2024-04-05 17:08:09 +0530

    Daily Tasks

---
+++ HTML/Day-20 03-04-24/index.js
@@ -0,0 +1,35 @@
+let num = 1242;
+console.log(num);
+
+let myName = 'Nikhil';
+console.log(myName);
+
+let male = true;
+console.log(male);
+
+let phone;
+console.log(phone);
+
+let val = null;
+console.log(val);
+
+let sym = Symbol('annefidety241');
+console.log(typeof sym);
+
+let arr = ['Nikhil', 'Sanjay', 'Ravi', 'Sid'];
+console.log(arr);
+
+let big = 13124124843279417392174921n;
+console.log(big);
+
+let a = '10' + 10;
+console.log(a);
+
+let b = +'10' + +'20';
+console.log(b);
+
+let c = 10 + 233;
+console.log(c);
+
+let d = 23 - 23;
+console.log(d);
